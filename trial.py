674348d7ef0da799957ea535ab10e0f0d646f8ba
shridharmanvi <shridharmanvi@gmail.com>
2014-12-04 16:20:20 -0600

    Added "like" statement to make the query return results

--- trial.py
+++ trial.py
@@ -6,10 +6,6 @@ from bson import json_util
 
 
 app = Flask(__name__)
-# to hold the current Version
-version = u'v1'
-# this will configure the MONGO_DBNAME
-# see: https://flask-pymongo.readthedocs.org/en/latest/
 app.config['MONGO_DBNAME'] = 'sphv'
 app.config['MONGO_USERNAME'] = 'sphv'
 app.config['MONGO_PASSWORD'] = '2s3WW6ut'
@@ -18,31 +14,25 @@ app.config['MONGO_PORT'] = '27017'
 
 mongo = PyMongo(app)
 
-#print app.config
-#db = MongoEngine(app)
 
-
-# our base test
-# {root}/
 @app.route('/')
 def hello_world():
     return 'World! How are you???'
 
 
-# {root}/api/v1/loans/
-# returns: all loans in the database
-@app.route('/trial/<drugname>', methods=['GET'])
+@app.route('/cse5320/<drugname>', methods=['GET'])
 def trial(drugname):
     if request.method == 'GET':
+        dn='/'+drugname+'/'
         print '1'
-        print drugname
-        rx = mongo.db.RxTerms.find({"FULL_GENERIC_NAME":drugname})
-        fda = mongo.db.RxTerms.find({"DRUGNAME":drugname})
+        print dn
+        rx = mongo.db.RxTerms.find({"DISPLAY_NAME":dn})
+        fda = mongo.db.fda1.find({"Display_Name":dn})
+        ndf= mongo.db.ndf.find({"Display_Name":dn})
         print "drugname"
         print '2'
         k_json = []
-        #k_json = [json.dumps(doc, default=json_util.default) for doc in k]
-        #k_json = [doc for doc in k]
+        
         try:
             for doc in rx:
                 json_dump = json.dumps(doc, default=json_util.default)
@@ -51,7 +41,19 @@ def trial(drugname):
             import pdb
             pdb.set_trace()
         print '3'
+        """
+        try:
+            for doc in fda:
+                json_dump = json.dumps(doc, default=json_util.default)
+                k_json.append(json_dump)
+        except:x=1
         
+        try:
+            for doc in fda:
+                json_dump = json.dumps(doc, default=json_util.default)
+                k_json.append(json_dump)
+        except:x=1
+        """
         return jsonify(result=k_json)
     else:
         return "Unsupported MimeType"
